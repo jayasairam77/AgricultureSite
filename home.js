dbc9616f89c107e7b7d069aec4e4ead42ed73d7e
jayasairam77 <jayasairam77@gmail.com>
2024-05-06 22:23:47 +0530

    first commit

---
+++ home.js
@@ -0,0 +1,6 @@
+// let url = "https://stackoverflow.com/";
+// let temp = document.createElement("a");
+// temp.href = url;
+// temp.download = 'file.pdf';
+// temp.dispatchEvent(new MouseEvent('click'));
+// document.body.appendChild(temp);
